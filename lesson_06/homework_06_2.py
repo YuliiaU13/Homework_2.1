a4f3ee46922dc4898ae1a1a0821c9cfe62aa89c4
YuliiaU13 <udovichenko.yuliya@gmail.com>
2024-06-11 22:26:15 +0300

    Виконані зміни 6.4

--- lesson_06/homework_06_2.py
+++ lesson_06/homework_06_2.py
@@ -2,10 +2,23 @@
 в якому є літера "h" (враховуються як великі так і маленькі).
 Цикл не повинен завершитися, якщо користувач ввів слово без букви "h".'''
 
+# while True:
+#     input_word_h = input('Введіть одне слово, в якому є літера "h" або "H": ')
+#     if ' ' not in input_word_h and ('h' in input_word_h or 'H' in input_word_h):
+#         print("Супер :) Ви ввели вірне слово: ", input_word_h)
+#         break
+#     else:
+#         print('Халепа :( Слово не містить необхідну літеру, або введено більше ніж одне слово.')
+
+# variant with two separate checks (continue) with 2 if and without else:
 while True:
     input_word_h = input('Введіть одне слово, в якому є літера "h" або "H": ')
-    if ' ' not in input_word_h and ('h' in input_word_h or 'H' in input_word_h):
-        print("Супер :) Ви ввели вірне слово: ", input_word_h)
+
+    if ' ' in input_word_h:
+        print('Халепа :( Введено більше ніж одне слово.')
+        continue
+    if 'h' in input_word_h.lower():
+        print('Супер :) Ви ввели вірне слово: ', input_word_h)
         break
-    else:
-        print('Халепа :( Слово не містить необхідну літеру, або введено більше ніж одне слово.')
+
+    print('Халепа :( Слово не містить необхідну літеру.')
---
+++ lesson_06/homework_06_4.py
@@ -0,0 +1,9 @@
+'''6.4 Є ліст з числами, порахуйте сумму усіх ПАРНИХ чисел в цьому лісті'''
+
+list_numbers = [12, 3, 44, 5, 0, 8, 93, 7, 4]
+
+sum_even = 0
+for number in list_numbers:
+    if number % 2 == 0:
+        sum_even += number
+print("Сума парних чисел у списку =", sum_even)
