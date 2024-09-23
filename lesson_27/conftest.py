72b54b3d547802eb8de529aba9805ca60161d67a
YuliiaU13 <udovichenko.yuliya@gmail.com>
2024-09-24 00:00:44 +0300

    lesson 27 conftest Chrome driver, requirements.txt

--- lesson_27/conftest.py
+++ lesson_27/conftest.py
@@ -1,9 +1,13 @@
-import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+import pytest
 
 
 @pytest.fixture(scope='session')
 def driver():
-    driver = webdriver.Chrome()
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service)
     yield driver
     driver.quit()
