3ff4693bce4074e5b4a56143230fe074d0da9c6e
YuliiaU13 <udovichenko.yuliya@gmail.com>
2024-09-24 00:52:01 +0300

    lesson_27 conftest.py

--- lesson_27/conftest.py
+++ lesson_27/conftest.py
@@ -1,13 +1,19 @@
+import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-import pytest
-
 
 @pytest.fixture(scope='session')
 def driver():
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+
+    service = Service('/usr/local/bin/chromedriver')
+    driver = webdriver.Chrome(service=service, options=options)
+
     yield driver
+
     driver.quit()
